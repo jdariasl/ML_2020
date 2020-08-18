4e2ed9f6f1974f114076b63546251f4b268cc8b9
jdariasl <jdariasl@gmail.com>
2020-08-18 13:39:59 -0500

    initi directory

---
+++ Labs/install-lib.sh
@@ -0,0 +1,8 @@
+pip3 install numpy
+pip3 install pixiedust
+pip3 install -U matplotlib
+pip3 install -U scikit-learn
+pip3 install pandas
+pip3 install qgrid
+jupyter nbextension enable --py --sys-prefix qgrid
+pip3 install mlxtend  

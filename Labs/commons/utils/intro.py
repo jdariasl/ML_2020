337cb1de36dc7c05dcaaa91e24b5c037588adc76
germaneduardo <germaneduardo.melo@gmail.com>
2020-08-23 20:14:04 -0500

    update the image

--- Labs/commons/utils/intro.py
+++ Labs/commons/utils/intro.py
@@ -2,7 +2,6 @@ from utils.general import Laboratory
 
 # for examples of exercise
 matriz_a_for =  np.array(range(100)).reshape((10,10))
-matriz_b_for = 1*np.ones((1,10))
-matriz_c_for = 45*np.ones(10)
+matriz_b_for = 2*np.ones((1,10))
 
 lab = Laboratory()

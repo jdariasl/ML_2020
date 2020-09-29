c1d828b21edd4135d2e1b78158fd4345885bbd0b
germaneduardo <germaneduardo.melo@gmail.com>
2020-09-28 19:35:42 -0500

    update general .py

--- Labs/commons/utils/general.py
+++ Labs/commons/utils/general.py
@@ -115,11 +115,13 @@ class Grader():
         for n,(k,v) in enumerate (self.results.items()):
             ans_list[n].value = f'{k}:{v}'
         print("resgister open exercises")
-        idx = 0
+
         for nn in range(1, open_questions+1):
             ans = eval(f"lab_obj.respuesta_{nn}").strip().encode('latin1')
-            ans_list[n+nn+idx].value = ans if ans !='' else 'no respuesta'
-            idx = idx+1
+            if nn == 1:
+                ans_list[n+nn].value = ans if ans !='' else 'no respuesta'
+            else:
+                ans_list[n+nn+1].value = ans if ans !='' else 'no respuesta'
 
         worksheet.update_cells(ans_list)
         

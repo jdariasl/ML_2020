4867d5e3e22d0426affe0c2f6622d36fdee7e66b
jdariasl <jdariasl@gmail.com>
2020-11-20 12:36:11 -0500

    text changes

---
+++ Labs/lab2/imports.py
@@ -0,0 +1,27 @@
+"""
+Este archivo es generado automaticamente.
+
+###### NO MODIFICAR #########
+
+# cualquier alteración del archivo
+# puede generar una mala calificación o configuracion
+# que puede repercutir negativamente en la 
+# calificación del laboratorio!!!!!
+
+###### NO MODIFICAR #########
+"""
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from general import Tester, Grader, Utils, unknow_error
+from scipy import stats
+import random
+import math
+import numpy.matlib
+import functools
+import scipy.io
+import inspect
+import time
+ut = Utils()

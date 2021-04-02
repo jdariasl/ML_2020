027be0b2e84a1087262fca1d2eb8592c195489ad
germaneduardo <germaneduardo.melo@gmail.com>
2021-04-02 17:51:45 -0500

    lab5 part1

--- Labs/commons/utils/lab5.py
+++ Labs/commons/utils/lab5.py
@@ -32,14 +32,32 @@ def generate_data(is_class = False, cols = 2):
     return (xx, yy)
 
 @unknow_error
-def test_experimentar_elman(func):
+def test_create_rnn_model (func):
+    code_to_look = [['1,look_back', "name='rnn_layer'", '.add(rnn_layer)',
+                     "loss='mean_absolute_error'"], 
+                   ['1,look_back', 'name="rnn_layer"', '.add(rnn_layer)',
+                     'loss="mean_absolute_error"']]
+    res2 = ut.check_code(code_to_look, func, msg = "**** recordar usar las funciones sugeridas ***", debug = False)
+    return (res2)
+
+
+
+@unknow_error
+def test_experimentar_rnn(func):
     xx, yy = generate_data(True)
     looksbacks = [1,2]
-    neu = [5,10]
+    neu = [5,7]
     cols =['lags', 'neuronas por capa', 'error de entrenamiento',
             'error de prueba']
 
     cols_errs = ['error de entrenamiento','error de prueba']
+
+    code_to_look = ['epochs=50', 'x=trainX', 'y=trainY', 
+                    'create_rnn_model', '.predict(trainX)', 'create_dataset',
+                    '.predict(testX)' , 'mean_absolute_error(testY', 
+                    ' mean_absolute_error(trainY']
+
+    res2 = ut.check_code(code_to_look, func, msg = "**** recordar usar las funciones sugeridas ***", debug = True)
 
     res = ut.test_experimento_oneset(func,  shape_val=(len(looksbacks)*len(neu), len(cols)), 
                                     col_error = cols_errs,
@@ -47,11 +65,6 @@ def test_experimentar_elman(func):
                                     data = xx,
                                     look_backs = looksbacks,
                                     hidden_neurons= neu)
-    code_to_look = ['nl.net.newelm', 'init.InitRand', '[num_hidden_neurons,1]', "nl.trans.TanSig()",  
-                    'nl.trans.PureLin()' , ".init", ".train(trainX", ".sim(trainX", 
-                    "epochs=1000", "goal=0.001", '.sim(testX',
-                    "mean_absolute_error(testY", "mean_absolute_error(trainY"] 
-    res2 = ut.check_code(code_to_look, func, msg = "**** recordar usar las funciones sugeridas ***", debug = False)
     return (res and res2)
 
 
@@ -72,7 +85,7 @@ def test_experimetar_mlp(func):
                                     look_backs = looksbacks,
                                     hidden_neurons= neu)
     code_to_look = ['MLPRegressor',  'hidden_layer_sizes=(num_hidden_neurons',
-                    "max_iter=1000", 'random_state=10', '.fit', '.predict',
+                    "max_iter=50", 'random_state=10', '.fit', '.predict',
                     "mean_absolute_error(testY", "mean_absolute_error(trainY"] 
     res2 = ut.check_code(code_to_look, func, msg = "**** recordar usar las funciones sugeridas ***", debug = False)
     return (res and res2)
@@ -95,7 +108,7 @@ def test_experimentar_LSTM(func):
                                     data = xx,
                                     look_backs = looksbacks,
                                     hidden_neurons= neu)
-    code_to_look = ['create_tf_model',   'epochs=100', 
+    code_to_look = ['create_lstm_model',   'epochs=50', 
                     '.predict(trainX)', '.predict(testX)',
                     "mean_absolute_error(testY", "mean_absolute_error(trainY"] 
     res2 = ut.check_code(code_to_look, func, msg = "**** recordar usar las funciones sugeridas ***", debug = False)
@@ -108,9 +121,10 @@ def part_1 ():
     dataset.columns =[ 'passengers']
     os.system("pip install neurolab")
     os.system("pip install statsmodels==0.12")
-    GRADER.add_test("ejercicio1", Tester(test_experimentar_elman))
-    GRADER.add_test("ejercicio2", Tester(test_experimetar_mlp))
-    GRADER.add_test("ejercicio3", Tester(test_experimentar_LSTM))
+    GRADER.add_test("ejercicio1", Tester(test_create_rnn_model))
+    GRADER.add_test("ejercicio2", Tester(test_experimentar_rnn))
+    GRADER.add_test("ejercicio3", Tester(test_experimetar_mlp))
+    GRADER.add_test("ejercicio4", Tester(test_experimentar_LSTM))
 
 
     return(GRADER, dataset)

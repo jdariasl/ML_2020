a52a2d17f8f9677fb1d3c416947c1e0750100a49
germaneduardo <germaneduardo.melo@gmail.com>
2021-04-10 16:53:38 -0500

    feat(lab6): mejores en el calificador y en los notebooks de los laboratorios

--- Labs/commons/utils/lab6.py
+++ Labs/commons/utils/lab6.py
@@ -12,20 +12,21 @@ Este archivo es generado automaticamente.
 """
 
 from imports import *
-from sklearn.model_selection import KFold
+from sklearn.model_selection import KFold, StratifiedKFold
 from sklearn.feature_selection import RFE
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
 from sklearn.decomposition import PCA
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, roc_auc_score
 import warnings
 import sklearn   
 warnings.filterwarnings('ignore')
 
 
 def generate_data(is_class = False, cols = 2):
-    yy = np.random.choice(2, 30) if is_class else 2*np.random.rand(60).reshape((int(60/cols),cols))
+    yy = np.random.choice(3, 30) if is_class else 2*np.random.rand(60).reshape((int(60/cols),cols))
     xx = np.vstack([np.random.rand(15, 3), 2*np.random.rand(15, 3)])
     return (xx, yy)
 
@@ -33,11 +34,12 @@ def generate_data(is_class = False, cols = 2):
 @unknow_error
 
 def test_entrenamiento_sin_seleccion_caracteristicas(function):
-    code_to_look = ['KFold(n_splits=', 'accuracy_score', '.predict', '.fit']
+    code_to_look = [['KFold(n_splits=', 'roc_auc_score', '.predict_proba', '.fit', "multi_class='ovo'"],
+                   ['KFold(n_splits=', 'roc_auc_score', '.predict_proba', '.fit', 'multi_class="ovr"']]
     res2 = ut.check_code(code_to_look, function, msg = "**** recordar usar las funciones sugeridas ***", debug = False)
     xx, yy = generate_data(is_class=True)
     a = function(3,xx,yy)
-    tests = {'*** No se retorno el modelo entrenado ***': isinstance(a[0], sklearn.svm._classes.SVC) ,
+    tests = {'*** No se retorno el modelo entrenado ***': len(a[0].classes_ ) >0 ,
              '*** No se retorno el error ***': a[1]>0.25,
              '*** No se retorno el desviacion estandar del error ***':  isinstance(a[2], float) and a[1]!=a[2],
              "*** No se retorno el tiempo de entrenamiento***": isinstance(a[3], float) and a[3]<=0.1
@@ -60,12 +62,11 @@ def test_recursive_feature_elimination_wrapper(function):
 
   xx =  np.array([[-1,0,0.0], [-1,-1,0.0], [1,1,0.1], [1,0.5,0.1], [0.1,0.1,0]])
   yy = np.array([1,1,2,2,2])
-  a = function(SVC(kernel="linear", C=1),1,xx, yy)
-
+  a = function(LogisticRegression(solver="liblinear", random_state=0),1,xx, yy)
   tests = {'*** No se retorno el modelo entrenado ***': isinstance(a[0], sklearn.feature_selection._rfe.RFE) ,
           '*** debes retornar la mascara del modelo RFE ***': a[1].sum() == 1,
           '*** retornar el ranking de los features ***':  (a[2] == np.array([1,2,3])).all(),
-          "*** No se retorno el modelo interno***": isinstance(a[3], sklearn.svm._classes.SVC),
+          "*** No se retorno el modelo interno***": isinstance(a[3], sklearn.linear_model._logistic.LogisticRegression),
           "*** No se retorno el tiempo de entrenamiento ***": isinstance(a[4], float) and a[4]<=0.1
             }
 
@@ -85,9 +86,9 @@ def test_experimentar(function):
   """
   xx, yy = generate_data(is_class=True)
   n_feats = [1,2]
-  n_sets = [3,5]
-  cols = ['CON_SEL', 'NUM_VAR', 'NUM_SPLITS', 'ERROR_VALIDACION',
-       'IC_STD_VALIDACION', 'T_EJECUCION']
+  n_sets = [2,3]
+  cols = ['CON_SEL', 'NUM_VAR', 'NUM_SPLITS', 'T_EJECUCION', 'ERROR_VALIDACION',
+       'IC_STD_VALIDACION']
   cols_errs = ['ERROR_VALIDACION', 'IC_STD_VALIDACION']
 
   res = ut.test_experimento_oneset(function,  shape_val=(len(n_feats)*len(n_sets)+len(n_sets), len(cols)), 
@@ -98,7 +99,7 @@ def test_experimentar(function):
                                    n_feats = n_feats,
                                    n_sets= n_sets)
   code_to_look = ['entrenamiento_sin_seleccion_caracteristicas', 'recursive_feature_elimination_wrapper', 
-                  'feature_numbers=f', '.predict(X_test)', 'mean', '.std', 'n_splits='] 
+                  'feature_numbers=f', '.predict_proba(X_test)', 'mean', '.std', 'n_splits='] 
   res2 = ut.check_code(code_to_look, function, msg = "**** recordar usar las funciones sugeridas ***", debug = False)
 
   return res2 and res
@@ -146,7 +147,7 @@ def test_entrenamiento_pca_seleccion_caracteristicas(function):
 def test_experimentar_PCA(function):
     xx, yy = generate_data(is_class=True)
     n_feats = [1,2]
-    cols = ['CON_SEL','NUM_VAR','ERROR_VALIDACION','IC_STD_VALIDACION','T_EJECUCION']
+    cols = ['CON_SEL','NUM_VAR', 'T_EJECUCION', 'ERROR_VALIDACION','IC_STD_VALIDACION']
     cols_errs = ['ERROR_VALIDACION', 'IC_STD_VALIDACION']
 
     res = ut.test_experimento_oneset(function,  shape_val=(len(n_feats)+1, len(cols)), 
@@ -176,8 +177,8 @@ def test_entrenamiento_lda_ext_caracteristicas(function):
     xx = np.array([[-1,0,0.0], [-1,-1,0.0], [1,1,0.1], [1,0.5,0.1], [0.1,0.1,0]])
     yy = np.array([1,1,2,2,2])
     a = function(1,xx, yy)
-    tests = {'***  hay un error en la funcion, revisa si se esta aplicando la media o el pca es entrenado y usado para transformar ***': a[0] == 0.4 and np.round(a[1],4) == 0.4899,
-            "*** No se retorno el tiempo de entrenamiento ***": isinstance(a[2], float) and a[2]<=0.1}
+    tests = {'*** hay un error en la funcion, revisa si se esta aplicando la media o el lda es entrenado y usado para transformar ***': a[1] == 0.4 and np.round(a[2],4) == 0.4899,
+            "*** No se retorno el tiempo de entrenamiento ***": isinstance(a[0], float) and a[0]<=0.1}
 
     test_res = ut.test_conditions_and_methods(tests)
     return(test_res and res2)
